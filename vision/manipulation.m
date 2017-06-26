20e3c02b386debae1ccb1f7321fe0c29f7109c14
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-06-26 14:49:09 -0700

    Add image manipulation program

---
+++ vision/manipulation.m
@@ -0,0 +1,30 @@
+clear;
+
+% Read image.png
+I = imread('image.png');
+
+% Display image
+imshow(I);
+
+% Get two mouse clicks, top left and bottom right
+[X,Y] = ginput(2);
+
+% Crop image to within those boundaries
+C = imcrop(I, [min(X) min(Y) diff(X) diff(Y)]);
+
+% Get new and original sizes
+orig_size = size(I);
+new_size = size(C);
+
+% Calculate new and original areas
+orig_area = orig_size(1) * orig_size(2);
+new_area = new_size(1) * new_size(2);
+
+% Calculate the appropriate scale
+scale = sqrt(orig_area/new_area);
+
+% Scale image so that area of final matches original
+C = imresize(C, scale);
+
+% Display final image
+imshow(C);
